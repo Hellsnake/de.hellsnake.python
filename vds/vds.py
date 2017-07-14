a87967fdf214cffe845eb01fe9343bd2be2c1eaf
Labor Labor <labor@cecert.local>
2017-07-14 12:16:41 +0200

    Make som changes for vds

--- vds/vds.py
+++ vds/vds.py
@@ -1,9 +1,40 @@
 import sys
+import visa
+import time
 
 def main():
+	tries = 1
+	stdout = sys.stdout
+	rm = visa.ResourceManager()
+	resourceList = rm.list_resources()
+	dev = rm.open_resource(resourceList[0], send_end=True, query_delay = 1.0)
 	checksum = ChecksumBuffer()
+	command = "DC;"
+	response = ''	
+	cs = checksum.get(command)
+	command = command + cs + "\n"
 
-	print(checksum.get('DE,15;'))
+
+	dev.encoding = 'cp437'
+
+	print("{0:s}".format(command))
+	response = dev.query(command)
+
+	while "VDS" not in response and tries <= 10:
+		dev.write(command)
+		response = dev.read()
+		stdout.write("Versuch {0:d}, Response: {1:s}\r".format(tries, response))
+		stdout.flush()
+		tries = tries + 1
+		time.sleep(2)
+
+	print(response)
+
+	dev.close()
+
+	
+
+	#print(checksum.get('BW;'))
 
 
 
@@ -20,13 +51,23 @@ class ChecksumBuffer:
 		sum = 0;
 		for b in bytelist:
 			sum = sum + b
+		#print(sum)
+		
 
 		# Offset als int (Hexadezimal = 100H) - errechnet Wert als int
 		result = int.from_bytes(self._offset, byteorder = 'big') - sum.to_bytes(2, byteorder='big')[1]
+
+		if result == 0:
+			result = result + 256
+		elif result == 10:
+			result = result + 266
+
+		#print(result)
 		c = result.to_bytes(1, byteorder = 'big', signed=False)
 
 		self._listOfChecksums[value] = c
-	
+		c = c.decode('cp437')
+
 		return c
 		
 	def get(self, value):
