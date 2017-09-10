8766575141fac443781035d1ec50de0c4e3ddcf1
Ronny B <ronny.bluethgen@gmail.com>
2017-09-10 22:20:00 +0200

    Improving the Checksum 'algorithm' for VDS

--- checksum.py
+++ checksum.py
@@ -7,17 +7,17 @@ class Checksum:
 		sumOfBytes = int()
 		offset = b'\x01\x00'
 		result = int()
+		byteCount = int()
 
 		for b in listOfbytes:
 			sumOfBytes = sumOfBytes + b
 
-		print("Checksum::getVDSChecksum sumOfBytes: '{sum:d}' ({bitCount:d}) for value '{val:s}'".format(sum = sumOfBytes, bitCount = sumOfBytes.bit_length(), val = stringValue))
+		byteCount = 1 if round(sumOfBytes.bit_length() / 8) == 0 else round(sumOfBytes.bit_length() / 8) + 1
 
-		result = int.from_bytes(offset, byteorder='big', signed=False) - sumOfBytes
-		print(result)
+		result = int.from_bytes(offset, byteorder='big', signed=False) - sumOfBytes.to_bytes(byteCount, byteorder="big")[1]
 
-
-Checksum.getVDSChecksum("DC;")
-
+		print("Checksum::getVDSChecksum sumOfBytes: '{sum:d}' (Byte Anzahl: {byteCount:d}) and result {result:d} for value '{val:s}'".format(sum = sumOfBytes, byteCount = byteCount, result=result, val = stringValue))
+		return result.to_bytes(2, byteorder="big").decode(encoding)
 
 
+print(Checksum.getVDSChecksum("AA,CC,DD,BB,DD,F,FF;"))
